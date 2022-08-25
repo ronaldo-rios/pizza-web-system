04b161efb4c9f317466318cea90dc49c8851ff84
Ronaldo Rios Espíndola <ronaldoriosespindola@gmail.com>
2022-08-25 17:09:49 -0300

    update modal and js pizzaInfo

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const cv = (el) => document.querySelectorAll(el)
 pizzaJson.map((item, index)=>{
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
 
+    pizzaItem.setAttribute('data-key', index)
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
@@ -17,6 +18,12 @@ pizzaJson.map((item, index)=>{
     //Evento ao clicar:
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault() //--> previnir a ação padrão de atualizar a tela
+        let key = e.target.closest('.pizza-item').getAttribute('data-key')
+        
+       c('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+       c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+       c('.pizzaBig img').src = pizzaJson[key].img
+
 
         c('.pizzaWindowArea').style.opacity = 0
         c('.pizzaWindowArea').style.display = 'flex'
