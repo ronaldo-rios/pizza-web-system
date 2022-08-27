28336d78ee006c03b2a6972b6308753da6c52579
Ronaldo Rios Espíndola <ronaldoriosespindola@gmail.com>
2022-08-27 16:25:19 -0300

    final commit

--- pizzas.js
+++ pizzas.js
@@ -1,9 +1,9 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Combinação de sabores que vai deixar você querendo mais. Impossível comer uma única fatia.'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Para quem não abre mão, temos a nossa tradicional de Calabresa com sabor caseiro que só aqui tem'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'De dar água na boca até a última mordida. Uma perfeita harmonia entre os quatro queijos'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descubra sabores combinados da favorita e queridinha pelos nossos clientes.'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Nada como uma pizza com bolas de sorvetes cremosas para aliviar o calor neste verão'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'A melhor combinação de sabores que você vai experimentar. Não há comparação'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Um misto do sabor doce do nosso chocolate com canela somado à massa macia'}
 ];
--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const c = (el) => {
 }
 
 const cv = (el) => document.querySelectorAll(el)
+
+
 //variável que armazena quantidade de itens:
 let modalQuantity = 1
+//Carrinho:
+let cart = []
+//Armazena qual pizza(objeto) é selecionado ao carrinho no evento de click pizzaInfo--addButton:
+let modalKey = 0
 
 //LISTAGEM DAS PIZZAS:
 
@@ -23,6 +29,8 @@ pizzaJson.map((item, index)=>{
         e.preventDefault() //--> previnir a ação padrão de atualizar a tela
         let key = e.target.closest('.pizza-item').getAttribute('data-key')
         modalQuantity = 1
+        modalKey = key
+
         //puxa do Json as informações do objeto e exibe quando abre o pop-up:
        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name
        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
@@ -67,3 +75,135 @@ function closeModal(){
 cv('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
     item.addEventListener('click', closeModal)
 })
+
+// Button de aumentar e diminuir quantidade de pizzas:
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+    modalQuantity++
+    c('.pizzaInfo--qt').innerHTML = modalQuantity
+})
+
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if(modalQuantity > 1){
+        modalQuantity--
+        c('.pizzaInfo--qt').innerHTML = modalQuantity
+    }
+    
+})
+
+//Seleção de tamanhos:
+cv('.pizzaInfo--size').forEach((size, sizeIndex) => {
+    size.addEventListener('click', () => {
+        c('.pizzaInfo--size.selected').classList.remove('selected')
+        size.classList.add('selected')
+    })
+       
+})
+
+//Adição ao carrinho:
+c('.pizzaInfo--addButton').addEventListener('click', () => {
+
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'))
+
+    let identifier = pizzaJson[modalKey].id + '@' + size
+
+    let verification = cart.findIndex((item) => {
+        return item.identifier == identifier
+    })
+    if(verification > -1){
+        cart[verification].qt += modalQuantity
+    }
+    else{
+        cart.push({
+            identifier,
+            id:pizzaJson[modalKey].id,
+            size,
+            qt:modalQuantity
+        })
+    }
+    updateCart()
+    closeModal()
+})
+
+
+//Abrir menu carrinho se houver pelo menos 1 item:
+c('.menu-openner').addEventListener('click', () => {
+    if(cart.length > 0){
+        c('aside').style.left = '0'
+    }    
+})
+
+//Button de fechar carrinho:
+c('.menu-closer').addEventListener('click', () => {
+    c('aside').style.left = '100vw'
+})
+
+
+
+//Mostra e atualiza através de cálculos o carrinho:
+function updateCart(){
+    
+    c('.menu-openner span').innerHTML = cart.length //--> atualiza o carrinho no mobile
+
+    if(cart.length > 0){
+        c('aside').classList.add('show')
+        c('.cart').innerHTML = ''
+
+        let subtotal = 0
+        let desconto = 0
+        let total = 0
+
+        for(let i in cart){
+            let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id)
+            subtotal += pizzaItem.price * cart[i].qt
+            let cartItem = c('.models .cart--item').cloneNode(true)
+            let pizzaSizeName;
+
+            switch(cart[i].size){
+                case 0:
+                    pizzaSizeName = 'P'
+                    break;
+                
+                case 1:
+                    pizzaSizeName = 'M'
+                    break;
+
+                case 2:
+                    pizzaSizeName = 'G'
+                    break;
+            }
+
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`
+
+            cartItem.querySelector('img').src = pizzaItem.img
+            cartItem.querySelector('.cart--item-name').innerHTML = pizzaName
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', () => {
+                if(cart[i].qt > 1){
+                    cart[i].qt--
+                }
+                else {
+                    cart.splice(i, 1)
+                }
+                updateCart()
+            })
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', () => {
+                cart[i].qt++
+                updateCart()
+            })
+
+            c('.cart').append(cartItem)
+        }
+
+        //Cálculo final do carrinho:
+        desconto = subtotal * 0.1
+        total = subtotal - desconto
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`
+    }
+    else {//caso a quantidade em carrinho seja 0 remove o aside:
+        c('aside').classList.remove('show')
+        c('aside').style.left = '100vw'
+    }
+}
